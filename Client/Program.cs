c043d05b3044a5e8e38b9521316e1990b27b802d
Amindlog <amind@bk.ru>
2022-04-11 23:28:19 +0400

    create project Laba1

---
+++ Client/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+System.Console.Write("Введите адресс сервера : ");
+IPAddress ipAddress = IPAddress.Parse(System.Console.ReadLine());
+
+System.Console.Write("Введите порт сервера : ");
+int port = int.Parse(System.Console.ReadLine());
+
+TcpClient tcpClient = new TcpClient();
+tcpClient.Connect(ipAddress, port);
+
+var stream = tcpClient.GetStream();
+System.Console.Write("Строка на оброботку : ");
+string str = System.Console.ReadLine();
+
+byte[] data = System.Text.Encoding.UTF8.GetBytes(str);
+
+stream.Write(data, 0, data.Length);
+
+byte[] returnData = new byte[256];
+int bytes = stream.Read(returnData, 0, returnData.Length);
+string str1 = System.Text.Encoding.UTF8.GetString(returnData, 0, bytes);
+System.Console.Write("Ответ от сервера : ");
+System.Console.WriteLine(str1);
+
+tcpClient.Close();
---
+++ Server/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Text.RegularExpressions;
+using System.Net;
+using System.Net.Sockets;
+TcpListener? server = null;
+var isStartingServer = true;
+
+System.Console.Write("Введите ipaddress server : ");
+string? ip = System.Console.ReadLine();
+IPAddress localAddr = IPAddress.Parse(ip);
+
+System.Console.Write("Введите порт для приложения : ");
+int port = int.Parse(System.Console.ReadLine());
+
+
+try
+{
+    server = new TcpListener(localAddr, port);
+    server.Start();
+    while (isStartingServer)
+    {
+        Console.WriteLine("Ожидание подключений... ");
+
+        // получаем входящее подключение
+        TcpClient client = server.AcceptTcpClient();
+        Console.WriteLine("Подключен клиент. Выполнение запроса...");
+
+        // получаем сетевой поток для чтения и записи
+        NetworkStream stream = client.GetStream();
+        byte[] data = new byte[256];
+        int bytes = stream.Read(data, 0, data.Length);
+        string message = System.Text.Encoding.UTF8.GetString(data, 0, bytes);
+        var result = new string(message.ToCharArray().Where(n => !char.IsDigit(n)).Where(n => !char.IsSymbol(n)).ToArray());
+        string backResult = "";
+
+        for (int i = 0; i < result.Length; i++)
+        {
+            if(result[i] != '*'){
+                backResult += result[i];
+            }
+        }
+
+        byte[] returnData = System.Text.Encoding.UTF8.GetBytes(backResult);
+        stream.Write(returnData, 0, returnData.Length);
+
+        System.Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Отправлено сообщение: {0}", backResult);
+        System.Console.ResetColor();
+        stream.Close();
+        client.Close();
+    }
+}
+catch (System.Exception e)
+{
+    System.Console.WriteLine(e.Message);
+}
+finally
+{
+    if (server != null) server.Stop();
+}
